f528c27d0b78b11fb9b3f75651d305f33b4b06e5
Dan Retegan <danretegan@yahoo.com>
2024-06-20 14:52:35 +0300

    Production & Development Modes

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist/assets"),
     filename: "bundle.js",
   },
-  mode: "development", //* Sau "production" în funcție de necesități
+
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
